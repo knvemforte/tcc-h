50db3ee5a1fc41ded22545bb8272ee441030ebde
kn <ga.rodrigo2005@gmail.com>
2022-08-30 17:07:14 -0300

    att

--- tcc-h/src/main/java/com/fiec/lpiiiback/models/repositories/CheckoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fiec.lpiiiback.models.repositories;
-
-import com.fiec.lpiiiback.models.entities.Checkout;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CheckoutRepository extends JpaRepository<Checkout, String> {
-}
