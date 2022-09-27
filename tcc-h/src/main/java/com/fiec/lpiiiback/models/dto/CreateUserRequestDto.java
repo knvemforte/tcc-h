aede6e63d396319816b73c9f4eaa887a19ace2bb
32920 <ga.rodrigo2005@gmail.com>
2022-09-27 20:11:26 -0300

    add firebase

--- tcc-h/src/main/java/com/fiec/lpiiiback/models/dto/CreateUserRequestDto.java
+++ tcc-h/src/main/java/com/fiec/lpiiiback/models/dto/CreateUserRequestDto.java
@@ -5,9 +5,11 @@ import lombok.Data;
 @Data
 public class CreateUserRequestDto {
     String name;
+    String lastName;
     String email;
     String password;
+    String phoneNumber;
     String cpf;
     String age;
-    String lastName;
+
 }
